6aa6081228c7ef048aea4ff577406270272b0bf2
XavierExCreator <xavier.ex.pro@gmail.com>
2025-04-17 13:54:37 -0400

    Adding a contact.html withint the website. connected the new html page onto index and latte pages. Added notes to cusotm js.

---
+++ js/custom.js
@@ -0,0 +1,5 @@
+// here in the custon js Selection, 
+// we'll be adding the functionality of a form!
+// What you'll do when you come here is that-
+// You'll log the tags from the HTML in order to have the form save the client information. 
+// You will than add a pop up section that says a thank you! 
